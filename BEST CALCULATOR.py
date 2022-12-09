74ee4f974bede1cb509bd876bbc784407a7437e9
SenkUU909 <118453009+SenkUU909@users.noreply.github.com>
2022-12-09 18:23:58 +0600

    Add files via upload

---
+++ BEST CALCULATOR.py
@@ -0,0 +1,109 @@
+print ("MEHETAB SPEACIAL GORIBER CALCULATOR")
+
+print ("FIRSTLY PLUS:")
+a=input ("1st Number:")
+b=input ("2nd Number:")
+print("")
+print ("THE RESULT IS :",float (a)+ float (b))
+
+
+#OUR FIRST WORK HAS DONE
+
+print ("")
+
+
+
+print ("SECOND MINUS")
+w = input ("1st number:")
+q = input ('2nd Number :')
+
+print ("")
+
+print ("THE RESULT IS :",float (w)-float (q))
+
+#EKHON MULTIPLY KORBO
+
+
+print ("")
+
+
+
+
+print ("THIRD MULTIPLY")
+wq = input ("1st NUMBER :")
+qw = input ("2nd NUMber : ")
+
+print ("")
+
+print ("THE RESULT IS:",float (wq)* float (qw))
+
+
+#DEVIDED BY KORBO
+
+print ("")
+
+
+print ("FOURTH DEVIDED ")
+
+ab = input ("1st NUMBER :")
+ba = input ("2nd NUMBER:")
+
+print ("")
+
+print ("THE RESULT IS :", float (ab)/ float (ba))
+
+
+
+#EKHON "TO THE POWER" er KAJ 
+
+print ("")
+
+print ("NOW 'TO THE POWER OF' ber korte :")
+ahh = input ("1st number :")
+uhh = input ("2nd number :")
+
+print ("")
+
+
+print ("THE RESULT IS :",float (ahh)** float (uhh))
+
+#EKHON VAGSESH BER KORAR LIGA
+
+print ("")
+
+print ("Vag SESH BER KORTE")
+
+puck = input ("1st Number :")
+pex = input ("2nd NUMBER :")
+
+print ("")
+
+print ("THE RESUL IS :",float (puck)% float (pex))
+
+
+# EKHON ROOT OVER BAIR KORTE
+
+print ("")
+
+print ("ROOT OVER BAIR KORTE")
+
+mehoo = input ("1ST NUMBER :")
+dipto = input ("2nd NUMBER :")
+
+print ("")
+
+print ("THE RESULT IS:",float (mehoo) // float (dipto) )
+
+
+
+# AMADER GORIBER CALCULATOR DONE
+
+print ("")
+print ("")
+
+print ("THANKS FOR USING MY GORIBER CALCULATOR")
+
+
+
+
+
